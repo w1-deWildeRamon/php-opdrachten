8fface681a883739071cc5a08fa510ed79c44cac
Ramon de Wilde <Ramon.dewilde@edu-kw1c.nl>
2020-02-11 12:29:25 +0100

    opdracht2.1.php taak 2

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
 <?php
-    echo "hello world";
+/*
+ * user: Ramon
+ * date: 11-02-2020
+ * time:
+ *file: index.php
+ */
 ?>
+<header>
+ <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="hoofdstuk%202/opdracht2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <ul>
+        <li>hoofdstuk 3
+            <ul>
+                <li>
+                    <a href="hoofdstuk%203/opdracht_3-1.php">opdracht 3.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk%203/opdracht_3-2.php">opdracht 3.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <ul>
+        <li>hoofdstuk 4
+            <ul>
+                <li>
+                    <a href="hoofdstuk%204/opdracht_4-1.php">opdracht 4.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk%204/opdracht_4-2.php">opdracht 4.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <ul>
+        <li>hoofdstuk 5
+            <ul>
+                <li>
+                    <a href="hoofdstuk%205/opdracht_5-1.php">opdracht 5.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk%205/opdracht_5-2.php">opdracht 5.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
