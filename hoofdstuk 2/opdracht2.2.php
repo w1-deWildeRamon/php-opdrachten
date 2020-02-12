5d2fde157ee0a2042a9ad7dde884e8c1682f3677
Ramon de Wilde <Ramon.dewilde@edu-kw1c.nl>
2020-02-12 15:27:40 +0100

    opdracht2.1.php taak 2

---
+++ hoofdstuk 2/opdracht2.2.php
@@ -0,0 +1,19 @@
+
+$text1 = "Hallo";
+$text2 = "een makkelijke taal";
+$text3 = "toch zo'n makkelijke taal";
+$text4 = "wat is";
+$text5 = "PHP";
+$text6 = "Nooit gedacht dat";
+$text7 = "De instalatie is best ingewikkeld";
+$text8 = "Fijn";
+$text9 = "?";
+$text10 = ".";
+$text11 = ",";
+$text12 = '< br >';
+$text13 = "is";
+$text14 = "vind je niet";
+$text15 = "toch";
+
+
+echo $text1 . $text11 . $text4 . $text5 . $text3 . $text2 . $text10 . $text7 . $text10 . ;
