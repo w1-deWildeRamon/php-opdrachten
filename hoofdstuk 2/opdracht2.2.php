29f4e0f0e6e1df31f21fb7f4d6b2c4d616ba9073
Ramon de Wilde <Ramon.dewilde@edu-kw1c.nl>
2020-02-18 11:31:35 +0100

    opdracht2.1.php taak 2

--- hoofdstuk 2/opdracht2.2.php
+++ hoofdstuk 2/opdracht2.2.php
@@ -1,4 +1,4 @@
-
+<?php
 $text1 = "Hallo";
 $text2 = "een makkelijke taal";
 $text3 = "toch zo'n makkelijke taal";
@@ -10,10 +10,10 @@ $text8 = "Fijn";
 $text9 = "?";
 $text10 = ".";
 $text11 = ",";
-$text12 = '< br >';
+$text12 = '<br>';
 $text13 = "is";
 $text14 = "vind je niet";
 $text15 = "toch";
 
 
-echo $text1 . $text11 . $text4 . $text5 . $text3 . $text2 . $text10 . $text7 . $text10 . ;
+echo $text1 . $text11 . $text4 . $text5 . $text3 . $text2 . $text12 . $text7 . $text10 . $text10. $text8 . $text15 . $text9 . $text6 . $text5 . $text3 . $text13;
